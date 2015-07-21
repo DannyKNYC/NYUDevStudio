b9329be1cf022a3ee1800839679f1922b38806e1
Danny Kim <daniel.c.kim@gmail.com>
2015-07-21 10:14:00 -0400

    Rube Goldberg Week 2B

---
+++ Week2B/Week2B Project/Assets/Scripts/TriggerDemo.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerDemo : MonoBehaviour {
+
+	bool didITriggerYet = false;
+	public GameObject particlesGo;
+
+	// Use this for initialization
+	void Start () {
+		particlesGo.SetActive (false);
+	
+	}
+
+
+	void OnTriggerEnter () {
+		didITriggerYet = true;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if ( didITriggerYet == true ) {
+			transform.Rotate ( 0f, 5f, 0f ); // if triggered, then spin
+			particlesGo.SetActive (true);
+		}
+
+
+	}
+
+	//whenever an object with a rigidbody and a collider
+	// enters the Trigger, the code here will execute
+	
+
+	                      
+}
