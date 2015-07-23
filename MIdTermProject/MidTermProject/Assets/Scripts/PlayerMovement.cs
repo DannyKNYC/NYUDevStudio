548ffe660ba745fb4b3eb021841b48e2053121d9
Danny Kim <daniel.c.kim@gmail.com>
2015-07-23 02:53:21 -0400

    Mid Term Player Move Test

---
+++ MIdTermProject/MidTermProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	public float speed = 5f;
+	public float turnSpeed = 90f;
+	
+	// Update is called once per frame
+	void Update () {
+		// "GetAxis" returns a float from -1 to 1
+		// from a "virtual joystick"...
+		float x = Input.GetAxis ("Horizontal"); // imagine [A] = -1, [D] = +1
+		float y = Input.GetAxis ("Vertical"); // imagine [W] = +1, [S] = -1
+		
+		transform.Translate (0f, 0f, y * speed * Time.deltaTime );
+		transform.Rotate ( 0f, x * turnSpeed * Time.deltaTime, 0f );
+
+		if (Input.GetKeyDown ("space")){
+			transform.Translate(Vector3.up * 150 * Time.deltaTime, Space.World);
+			 } 
+
+		
+	}
+}
